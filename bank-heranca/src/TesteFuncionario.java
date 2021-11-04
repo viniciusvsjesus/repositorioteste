cc1a85057a749a0fe160815fc821c02ba8f632d7
viniciusvsjesus <viniciusvsprofissional@gmail.com>
2021-11-03 21:15:37 -0300

    Primeiro commit

---
+++ bank-heranca/src/TesteFuncionario.java
@@ -0,0 +1,17 @@
+
+public class TesteFuncionario {
+public static void main(String[] args) {
+Funcionario vinicius = new Funcionario();
+vinicius.setNome("Vinicius Silva");
+vinicius.setCpf("123456");
+vinicius.setSalario(10000);
+
+System.out.println(vinicius.getBonificacao());
+
+
+
+TesteSet teste = new TesteSet();
+
+
+}
+}
